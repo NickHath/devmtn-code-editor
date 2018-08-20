1a737d637a316ece25c3d47139371e38b45bc441
Mira Jones <meschoolstuffya@gmail.com>
2018-08-20 14:10:54 -0600

    Fixed the json that shows up

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,66 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './CodeEditor.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CodeEditor.css";
 
 // all tokens will be imported here
-import VarKeywordToken from '../Token/tokens/VarKeywordToken';
-import VarNameToken from '../Token/tokens/VarNameToken';
-import StringToken from '../Token/tokens/StringToken';
-import OperatorToken from '../Token/tokens/OperatorToken';
+import VarKeywordToken from "../Token/tokens/VarKeywordToken";
+import VarNameToken from "../Token/tokens/VarNameToken";
+import StringToken from "../Token/tokens/StringToken";
+import OperatorToken from "../Token/tokens/OperatorToken";
 
 // object that maps from .json types to their appropriate class constructors
 const componentNames = {
-  'VarKeyword': VarKeywordToken,
-  'VarName': VarNameToken,
-  'String': StringToken,
-  'Operator': OperatorToken
+  VarKeyword: VarKeywordToken,
+  VarName: VarNameToken,
+  String: StringToken,
+  Operator: OperatorToken
 };
 
 export default props => {
-  const tokenComponents = props.tokens.map(token=> {
+  const tokenComponents = props.tokens.map(token => {
     // the .json might include a string for formatting, like '{\n'
-    if (typeof token === 'string') {
+    if (typeof token === "string") {
       return token;
     } else {
       // or it could be an object specifing a token component
       const { id, type, defaultValue, locked, expected } = token;
       // this stores the correct component in a temporary variable to be rendered
       const CurrentToken = componentNames[type];
-      return <CurrentToken key={ id } defaultValue={ defaultValue } locked={ locked } expected={ expected } />
+      return (
+        <CurrentToken
+          key={id}
+          defaultValue={defaultValue}
+          locked={locked}
+          expected={expected}
+        />
+      );
     }
   });
 
   return (
-    <div className='code-editor-wrapper'>
-      <div className='nav-links'>
-        <Link className='link' to='/'>
-          <p className='nav-button'>Home<br/>{'<'}</p>
+    <div className="code-editor-wrapper">
+      <div className="nav-links">
+        <Link className="link" to="/">
+          <p className="nav-button">
+            Home
+            <br />
+            {"<"}
+          </p>
         </Link>
-        {
-          props.next ?
-            <Link className='link' to={ `/1/${props.next}` }>
-              <p className='nav-button'>Next<br/>{'>'}</p>
-            </Link> :
-            null
-        }
+        {props.next ? (
+          <Link className="link" to={`/1/${props.next}`}>
+            <p className="nav-button">
+              Next
+              <br />
+              {">"}
+            </p>
+          </Link>
+        ) : null}
       </div>
-      <div className='editor'>
-        <div className='instructions-code'>
+      <div className="editor">
+        <div className="instructions-code">
           {/* WHERE THE MAGIC HAPPENS */}
-          <h1 className='title'>{ props.title }</h1>
-          <p className='description'>{ props.description }</p>
-          { tokenComponents }
+          <h1 className="title">{props.title}</h1>
+          <p className="description">{props.description}</p>
+          {tokenComponents}
         </div>
-        <div className='lesson-json'>
-          <span style={{ color: 'red' }}>JSON used to generate this code (each object is a component):</span> 
-          <br/>
-          { JSON.stringify(props.tokens) };
-          <br/>
-          <span style={{ color: 'red' }}>Number of components generated:</span> 
-          <br/>
-          { props.tokens.filter(token => typeof token !== 'string').length }
+        <div className="lesson-json">
+          <span style={{ color: "red" }}>
+            JSON used to generate this code (each object is a component):
+          </span>
+          <br />
+          {props.tokens.map(el => {
+            return (
+            el.id ? 
+              <div>
+                <p>
+                  id: {el.id}, type: {el.type}, locked: {el.locked},
+                  defaultValue: {el.defaultValue}
+                </p>
+              </div>
+              : 
+              <div>
+                <p>punctuation: {el.defaultValue}</p>
+              </div>
+            );
+          })}
+          ;<br />
+          <span style={{ color: "red" }}>Number of components generated:</span>
+          <br />
+          {props.tokens.filter(token => typeof token !== "string").length}
         </div>
       </div>
     </div>
   );
-} 
+};
--- src/components/Token/tokens/VarKeywordToken.js
+++ src/components/Token/tokens/VarKeywordToken.js
@@ -1,28 +1,43 @@
-import React from 'react';
-import Token from '../Token';
+import React from "react";
+import Token from "../Token";
 
 export default class VarKeywordToken extends Token {
   constructor() {
     super();
     this.state = {
-      input: '',
-      display: ''
+      input: "",
+      display: ""
     };
   }
 
   render() {
     const { locked, defaultValue, expected } = this.props;
     const { input, display } = this.state;
-    const boxStyle = ({ width: expected && `${expected.length}em`, borderColor: `#FD5FF1` });
+    const boxStyle = {
+      width: expected && `${expected.length}em`,
+      borderColor: `#FD5FF1`
+    };
     return (
-      <div className='token var-keyword' onClick={ () => this.setState({ display: '' }) }>
-        { display || defaultValue || <input autoFocus 
-                                            onBlur={ this.validateToken.bind(this, input, expected, 'string') } 
-                                            onKeyUp={ e => e.keyCode === 13 ? this.validateToken.call(this, input, expected, 'string') : null }
-                                            defaultValue={ input } 
-                                            className='input-box' 
-                                            onChange={ e => this.setState({ input: e.target.value }) } 
-                                            style={ boxStyle }/>}
+      <div
+        className="token var-keyword"
+        onClick={() => this.setState({ display: "" })}
+      >
+        {display ||
+          defaultValue || (
+            <input
+              autoFocus
+              onBlur={this.validateToken.bind(this, input, expected, "string")}
+              onKeyUp={e =>
+                e.keyCode === 13
+                  ? this.validateToken.call(this, input, expected, "string")
+                  : null
+              }
+              defaultValue={input}
+              className="input-box"
+              onChange={e => this.setState({ input: e.target.value })}
+              style={boxStyle}
+            />
+          )}
       </div>
     );
   }
